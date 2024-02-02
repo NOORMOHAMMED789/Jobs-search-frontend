32752c6ff25fcc74c58739b0daab12be338e4bcb
Noor Mohammed <noormohammedknr2@gmail.com>
2024-02-02 22:06:22 +0530

    error fixed

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -6,9 +6,7 @@ export default function Header() {
     <div className="h-[80px] bg-[#303B54] flex justify-around items-center">
       <div className="flex justify-between items-center w-[80%]">
         <div className="text-[#fff] text-[24px]">Jobs</div>
-        <div>
-          <SearchField />
-        </div>
+        <SearchField />
         <div>Icon</div>
       </div>
       <div>Notification icon</div>
--- src/components/SearchField.jsx
+++ src/components/SearchField.jsx
@@ -1,13 +1,20 @@
+import Image from "next/image";
 import React from "react";
 
 export default function SearchField() {
   return (
-    <div className="w-[490px] relative h-[48px] bg-[#242D40] rounded-full flex justify-between items-center p-8">
+    <div className="w-[490px] h-[48px] bg-[#242D40] rounded-full flex justify-between items-center p-7">
       <input
         type="text"
         className="w-[100%] h-[25px] bg-transparent text-[#fff] text-[18px] outline-none"
       />
-      <div className="absolute right-[20px]">search</div>
+      <Image
+        src="/Images/search-icon.png"
+        className=""
+        width={18.37}
+        height={18.31}
+        alt="image"
+      />
     </div>
   );
 }
