e1292ab8ea377f5749660794d6f40ca5404668f2
Noor Mohammed <noormohammedknr2@gmail.com>
2024-02-03 11:49:31 +0530

    UI in Progress

--- src/components/Jobs.jsx
+++ src/components/Jobs.jsx
@@ -5,7 +5,7 @@ export default function Jobs() {
   return (
     <div className="bg-[#1D2331] rounded-2xl">
       <div className="mt-[30px] p-10">
-        <div className=" bg-gradient-to-r from-gray-500 uppercase p-2 text-[#fff] text-[16px] leading-[20px] font-light pl-[15px] rounded-full">
+        <div className="ml-[15px] bg-gradient-to-r from-gray-500 uppercase p-2 text-[#fff] text-[16px] leading-[20px] font-light pl-[15px] rounded-full">
           Jobs
         </div>
         <div className="p-2 mt-[25px] pl-[15px]">
